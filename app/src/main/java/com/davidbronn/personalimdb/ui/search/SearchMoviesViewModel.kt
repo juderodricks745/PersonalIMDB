0270e718b075afcd48742b9bea465e2361d8e95e
nbt989 <jude.rodricks@paynearby.in>
2020-01-14 09:54:54 +0530

    - Added Progress in search

--- app/src/main/java/com/davidbronn/personalimdb/ui/search/SearchMoviesViewModel.kt
+++ app/src/main/java/com/davidbronn/personalimdb/ui/search/SearchMoviesViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.davidbronn.personalimdb.models.network.ResultsItem
 import com.davidbronn.personalimdb.repository.searchmovies.SearchMoviesRepository
 import com.davidbronn.personalimdb.utils.Result
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.*
 
@@ -14,22 +15,26 @@ import java.util.*
  */
 class SearchMoviesViewModel(private val repository: SearchMoviesRepository) : ViewModel() {
 
+    val progress = MutableLiveData<Boolean>()
     val movieResults = MutableLiveData<List<ResultsItem?>>()
 
     /**
      * Fetch movies by the search text
      */
     fun fetchMovies(searchText: String) {
+        progress.value = true
         viewModelScope.launch {
+            delay(1000)
             when (val result = repository.fetchMovies(searchText, "true")) {
                 is Result.Success -> {
+                    progress.value = false
                     movieResults.value = result.data ?: Collections.emptyList()
                 }
                 is Result.Error -> {
-
+                    progress.value = false
                 }
                 Result.Loading -> {
-
+                    progress.value = false
                 }
             }
         }

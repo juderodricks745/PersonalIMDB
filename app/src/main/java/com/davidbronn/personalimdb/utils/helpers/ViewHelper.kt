b77a9de1f92ef470bbfbefdf561a8809d1d52931
nbt989 <jude.rodricks@paynearby.in>
2020-03-26 23:12:47 +0530

    View Similar movies & cast with animation

--- app/src/main/java/com/davidbronn/personalimdb/utils/helpers/ViewHelper.kt
+++ app/src/main/java/com/davidbronn/personalimdb/utils/helpers/ViewHelper.kt
@@ -10,23 +10,16 @@ import android.view.animation.ScaleAnimation
  * Created by Jude on 07/March/2020
  */
 
-fun View.startOverShootAnimation(time: Long) {
-    this.animate().apply {
-        startDelay = time
-        interpolator = OvershootInterpolator()
-        alpha(1f)
-        start()
-    }
-}
-
 fun View.scale() {
-    val anim = ScaleAnimation(
-        0f, 0f, // Start and end values for the X axis scaling
-        1f, 1f, // Start and end values for the Y axis scaling
-        Animation.RELATIVE_TO_SELF, 0f, // Pivot point of X scaling
-        Animation.RELATIVE_TO_SELF, 1f
-    ) // Pivot point of Y scaling
-    anim.fillAfter = true // Needed to keep the result of the animation
-    anim.duration = 1000
-    this.startAnimation(anim)
+    ScaleAnimation(
+        0f, 1f,
+        0f, 1f,
+        Animation.RELATIVE_TO_PARENT, 0.5f,
+        Animation.RELATIVE_TO_PARENT, 0.5f
+    ).apply {
+        duration = 600
+        interpolator = OvershootInterpolator()
+        this@scale.startAnimation(this)
+        this@scale.visibility = View.VISIBLE
+    }
 }
